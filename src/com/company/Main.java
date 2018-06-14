afc3235f027ff461b923bc12a9b2d85a28a45bc4
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-06-14 14:35:25 +0300

    нечетные

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int a = 0;
 
+        while (a <= 100) {
+            if (a % 2 != 0)
+            System.out.println(a);
+            a++;
 
-        for (int a = 2; a < 101; a++) {
+        }
+
+       /* for (int a = 2; a < 101; a++) {
             if (a % 2 == 0)
             System.out.println(a);
-        }
+        }*/
     }
 }
