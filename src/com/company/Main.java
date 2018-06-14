e48e61797de5be80f937ef1b91a9d63e90f1a699
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-06-14 13:50:07 +0300

    new

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 3;
+        switch (a) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            case 4:
+                System.out.println("4");
+                break;
+        }
+
+
+    }
+}
