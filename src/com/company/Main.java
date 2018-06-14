c91163a4e3cac8c4f08b48a26e2d1f6b44e7e51c
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-06-14 14:16:28 +0300

    while

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
+int a=0;
+        while (a<10) {
+            System.out.println(a);
+            a++;
         }
+
 
     }
 }
